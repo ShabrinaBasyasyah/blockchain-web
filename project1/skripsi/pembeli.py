87654caeddfa25096e180f0b7483ee72691602e2
Shabrina Basyasyah <77041087+ShabrinaBasyasyah@users.noreply.github.com>
2024-02-26 15:49:13 +0700

    Delete project1/skripsi directory

--- project1/skripsi/pembeli.py
+++
@@ -1,9 +0,0 @@
-class pembeli:
-    def __init__(self, id_pembeli, nama_perusahaan_pembeli, nama_pltu, unit_ke, password, saldo):
-        self.id_pembeli = id_pembeli
-        self.nama_perusahaan_pembeli = nama_perusahaan_pembeli
-        self.nama_pltu = nama_pltu
-        self.unit_ke = unit_ke
-        self.password = password
-        self.saldo = saldo
-        
